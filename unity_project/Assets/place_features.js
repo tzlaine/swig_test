fe30f5f2cc40b2ca03e3dd9d49b93dd769922ec7
Intific, Inc. by T. Zachary Laine <tzlaine@intific.com>
2013-12-16 09:09:37 -0600

    Batch capitol star rending into a single call.

--- unity_project/Assets/place_features.js
+++ unity_project/Assets/place_features.js
@@ -13,7 +13,14 @@ function place_features (h : hex, m : map_t)
 
     var scale : float = 1.0;
 
-    if (h.feature == 'MIN' || h.feature == 'MAJ') {
+    if (h.feature == 'capitol') {
+        var capitol : procedural_star = Instantiate(star);
+        capitol.init(h.owner_id, game_data_.capitol_star_points(h.owner));
+        capitol.transform.position = place_tiles_.hex_center(hex_coord(h.x, h.y));
+        scale = 0.5;
+        capitol.transform.localScale = Vector3(scale, scale, scale);
+        capitol.renderer.sharedMaterial.renderQueue = 30;
+    } else if (h.feature == 'MIN' || h.feature == 'MAJ') {
         var planet : GameObject = Instantiate(circle);
         planet.transform.position = place_tiles_.hex_center(hex_coord(h.x, h.y));
         scale = h.feature == 'MIN' ? 0.2 : 0.3;
@@ -25,13 +32,12 @@ function place_features (h : hex, m : map_t)
         }
         mesh.uv2 = uv2;
         planet.renderer.sharedMaterial.renderQueue = 30;
-    } else if (h.feature == 'capitol') {
-        var capitol : procedural_star = Instantiate(star);
-        capitol.init(h.owner_id, game_data_.capitol_star_points(h.owner));
-        capitol.transform.position = place_tiles_.hex_center(hex_coord(h.x, h.y));
-        scale = 0.5;
-        capitol.transform.localScale = Vector3(scale, scale, scale);
-        capitol.renderer.sharedMaterial.renderQueue = 30;
+    } else if (h.feature == 'SB') {
+        var sb = new GameObject();
+        // TODO
+    } else if (h.feature == 'BATS') {
+        var bats = new GameObject();
+        // TODO
     }
 }
 
@@ -50,23 +56,22 @@ function Start ()
     }
 }
 
-/*
-private var hexes_combined = false;
+private var features_combined = false;
 
-function combine_hexes (mesh_filter : MeshFilter, components : Component[]) : boolean
+function combine_features (mesh_filter : MeshFilter, stars : Component[]) : boolean
 {
-    if (!components.Length)
+    if (!stars.Length)
         return false;
 
     var mesh = new Mesh();
     mesh_filter.mesh = mesh;
 
-    var combine : CombineInstance[] = new CombineInstance[components.Length];
-    for (var i = 0; i < components.Length; i++){
-        var hex_mesh_filter : MeshFilter = components[i].GetComponent(MeshFilter);
+    var combine : CombineInstance[] = new CombineInstance[stars.Length];
+    for (var i = 0; i < stars.Length; i++){
+        var hex_mesh_filter : MeshFilter = stars[i].GetComponent(MeshFilter);
 	combine[i].mesh = hex_mesh_filter.mesh;
 	combine[i].transform = hex_mesh_filter.transform.localToWorldMatrix;
-	hex_mesh_filter.gameObject.active = false;
+	hex_mesh_filter.gameObject.SetActive(false);
     }
     mesh.CombineMeshes(combine);
 
@@ -75,11 +80,10 @@ function combine_hexes (mesh_filter : MeshFilter, components : Component[]) : bo
 
 function Update ()
 {
-    if (!hexes_combined &&
-        combine_hexes(GetComponent(MeshFilter),
-                      FindObjectsOfType(procedural_hex))) {
-        renderer.material.renderQueue = 10;
-        hexes_combined = true;
+    if (!features_combined &&
+        combine_features(GetComponent(MeshFilter),
+                         FindObjectsOfType(procedural_star))) {
+        renderer.material.renderQueue = 30;
+        features_combined = true;
     }
 }
-*/
