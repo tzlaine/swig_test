8898250a4c8a87ac40d711abf93b5d5c25e81ff6
Intific, Inc. by T. Zachary Laine <tzlaine@intific.com>
2013-12-24 13:36:00 -0600

    Add script to make the entire units.json file from repeated calls to csv_to_json.py.

---
+++ sits/make_units.json.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import subprocess
+
+files = [
+    ('msit_feds.csv', 'FED'),
+    ('msit_gorns.csv', 'GOR'),
+    ('msit_hydrans.csv', 'HYD'),
+    ('msit_klingon.csv', 'KLI'),
+    ('msit_kzinti.csv', 'KZI'),
+    ('msit_lyran.csv', 'LYR'),
+    ('msit_orion.csv', 'ORI'),
+    ('msit_rom.csv', 'ROM'),
+    ('msit_tholian.csv', 'THO')
+]
+
+print '{'
+for i in range(0, len(files)):
+    output = subprocess.check_output(['./csv_to_json.py', files[i][0], files[i][1]]);
+    if i == len(files) - 1:
+        print output[0:-2]
+    else:
+        print output[0:-1]
+print '}'
