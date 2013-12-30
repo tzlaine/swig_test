9c303955bfd2df4a463405db6dbeb86ddc00db68
Intific, Inc. by T. Zachary Laine <tzlaine@intific.com>
2013-12-30 14:44:22 -0600

    Initial sketch of new game code, including a save system.

--- unity_project/Assets/game_state.js
+++ unity_project/Assets/game_state.js
@@ -1,25 +1,88 @@
 ﻿#pragma strict
 
+import System.IO;
 
-function clear ()
+@script SerializeAll
+
+@DoNotSerialize
+var game_data_ : game_data;
+
+@DoNotSerialize
+private var scenario : SimpleJSON.JSONNode;
+
+private var in_setup : boolean = true;
+private var turn : int;
+private var player_nations : String[];
+private var setting_up_nation : int = -1;
+private var game_guid : String;
+
+private function turn_to_str (turn_ : int) : String
 {
+    var retval : String = turn_ / 10 == 0 ? 'Spring' : 'Fall';
+    retval += ', Y' + Mathf.FloorToInt(turn_ / 10.0);
 }
 
-function new_game (scenario : SimpleJSON.JSONNode, player_config : Dictionary.<String, boolean>)
+private function save_description () : String
 {
-    print('TODO: Start new game.');
-    for (var config : System.Collections.Generic.KeyValuePair.<String, boolean> in
-         player_config) {
-        print(config.Key + ' ' + (config.Value ? 'human' : 'AI'));
+    var retval : String = scenario['name'];
+    if (in_setup)
+        retval += ' - Initial setup';
+    else
+        retval += ' - ' + turn_to_str(turn);
+    return retval;
+}
+
+private function save ()
+{
+    var info = new DirectoryInfo(Application.persistentDataPath);
+    var files = info.GetFiles();
+    var matching_guid_saves = new Array();
+    for (file in files) {
+        var filename : String = file.ToString();
+        if (filename.StartsWith(game_guid))
+            matching_guid_saves.Push(filename);
+    }
+
+    var save_filename : String = game_guid;
+    JSONLevelSerializer.SerializeLevelToFile(game_guid + save_description());
+
+    for (match in matching_guid_saves) {
+        File.Delete(match);
     }
 }
 
-function Start ()
+function clear ()
 {
+    // TODO
+}
 
+function new_game (scenario_ : SimpleJSON.JSONNode, config : Dictionary.<String, boolean>)
+{
+    scenario = scenario_;
+
+    turn = game_data.parse_turn(scenario['start turn']);
+
+    var played_nations = new Array();
+    for (var conf : System.Collections.Generic.KeyValuePair.<String, boolean> in
+         config) {
+        // TODO: Restore this later; for now, we play hotseat-only. if (conf.Value)
+            played_nations.Push(conf.Key);
+    }
+    game_data_.load_data(scenario);
+
+    game_guid = System.Guid.NewGuid().ToString();
+
+    player_nations = new String[played_nations.length];
+    for (var i = 0; i < played_nations.length; ++i) {
+        player_nations[i] = played_nations[i];
+    }
+
+    // TODO: Show note about initial setup.
+
+    save();
 }
 
 function Update ()
 {
-
+    // TODO
 }
