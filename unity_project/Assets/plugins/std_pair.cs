9fa9e0bd105781dfbfafc5bf8e9a5e92a9ef39d8
Intific, Inc. by T. Zachary Laine <tzlaine@intific.com>
2014-01-03 16:20:48 -0600

    Add pair<T,U> C# type modelled after std::pair.

---
+++ unity_project/Assets/plugins/std_pair.cs
@@ -0,0 +1,34 @@
+﻿public class pair<T, U>
+{
+    public pair () {}
+
+    public pair (T first, U second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    public override bool Equals (object rhs_)
+    {
+        if (rhs_ == null)
+            return false;
+        pair<T, U> rhs = rhs_ as pair<T, U>;
+        if (rhs == null)
+            return false;
+
+        return
+            (first == null && rhs.first == null || first.Equals(rhs.first)) &&
+            (second == null && rhs.second == null || second.Equals(rhs.second));
+    }
+
+    public override int GetHashCode ()
+    {
+        int retval = first == null ? 0 : first.GetHashCode();
+        if (second != null)
+            retval ^= second.GetHashCode();
+        return retval;
+    }
+
+    public T first;
+    public U second;
+};
