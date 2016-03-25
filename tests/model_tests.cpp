e9cecc8b494bc327310e3c7d224654aad67871db
Zach Laine <whatwasthataddress@gmail.com>
2016-03-24 20:10:26 -0500

    nations.json -> nations.txt

--- tests/model_tests.cpp
+++ tests/model_tests.cpp
@@ -5,5 +5,5 @@
 
 TEST(whole_model_ops, init)
 {
-    EXPECT_TRUE(init_model(nations_json_string, map_json_string, oob_json_string));
+    EXPECT_TRUE(init_model(nations_pbtxt_string, map_json_string, oob_json_string));
 }
