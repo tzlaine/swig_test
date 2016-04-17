347bbb85db24b39052f8bee9f138a5ce86299154
Zach Laine <whatwasthataddress@gmail.com>
2016-04-17 16:15:56 -0500

    Add empty hex_component.{h,cpp}.

---
+++ unreal_project/Spaceport/Source/Spaceport/hex_component.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Spaceport.h"
+#include "hex_component.h"
+
+
+// Sets default values for this component's properties
+Uhex_component::Uhex_component()
+{
+    bWantsBeginPlay = false;
+    PrimaryComponentTick.bCanEverTick = false;
+}
---
+++ unreal_project/Spaceport/Source/Spaceport/hex_component.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "hex_component.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class SPACEPORT_API Uhex_component : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:	
+    // Sets default values for this component's properties
+    Uhex_component();
+};
