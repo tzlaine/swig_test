f0b56f4ab111deef1e52e64f28425ded352bb674
Zach Laine <whatwasthataddress@gmail.com>
2016-04-17 20:03:05 -0500

    Add hex Actor subclass.

---
+++ unreal_project/Spaceport/Source/Spaceport/hex.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "hex.generated.h"
+
+UCLASS()
+class SPACEPORT_API Ahex : public AActor
+{
+    GENERATED_BODY()
+public:	
+    Ahex();
+
+    inline class UStaticMeshComponent* mesh() const
+    { return mesh_; }
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Hex", meta = (AllowPrivateAccess = "true"))
+    class UStaticMeshComponent* mesh_;
+};
