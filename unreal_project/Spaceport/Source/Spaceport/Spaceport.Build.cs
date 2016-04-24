a08dbbb7f1eb35d74efc175638e1255db9a579df
Zach Laine <whatwasthataddress@gmail.com>
2016-04-23 21:57:09 -0500

    Migrate json2pb fully into UE4 tree.  This requires a 64-bit, release build to be done from the project root and installed.

--- unreal_project/Spaceport/Source/Spaceport/Spaceport.Build.cs
+++ unreal_project/Spaceport/Source/Spaceport/Spaceport.Build.cs
@@ -1,26 +1,104 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class Spaceport : ModuleRules
 {
-	public Spaceport(TargetInfo Target)
-	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+    private string ModulePath
+    {
+        get { return Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name)); }
+    }
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
+    }
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-		// if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
-		// {
-		//		if (UEBuildConfiguration.bCompileSteamOSS == true)
-		//		{
-		//			DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
-		//		}
-		// }
-	}
+    public Spaceport(TargetInfo Target)
+    {
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
+
+        // Uncomment if you are using Slate UI
+        // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64))
+        // {
+        //		if (UEBuildConfiguration.bCompileSteamOSS == true)
+        //		{
+        //			DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
+        //		}
+        // }
+
+        PublicIncludePaths.Add(Path.Combine(ModulePath, "../../../../", "boost_1_60_0"));
+
+        LoadProtobuf(Target);
+        LoadJansson(Target);
+    }
+
+    public bool LoadProtobuf(TargetInfo Target)
+    {
+        string libraries_path = Path.Combine(ThirdPartyPath, "Protobuf", "Libraries");
+
+        string protobuf_lib;
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            protobuf_lib = "libprotobufd.lib";
+            if (Target.Configuration == UnrealTargetConfiguration.Development ||
+                Target.Configuration == UnrealTargetConfiguration.Shipping ||
+                Target.Configuration == UnrealTargetConfiguration.Test) {
+                protobuf_lib = "libprotobuf.lib";
+            }
+        }
+        else // TODO: Untested!
+        {
+            protobuf_lib = "libprotobufd.a";
+            if (Target.Configuration == UnrealTargetConfiguration.Development ||
+                Target.Configuration == UnrealTargetConfiguration.Shipping ||
+                Target.Configuration == UnrealTargetConfiguration.Test) {
+                protobuf_lib = "libprotobuf.a";
+            }
+        }
+
+        PublicAdditionalLibraries.Add(Path.Combine(libraries_path, protobuf_lib));
+        PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "Protobuf", "Includes"));
+        //Definitions.Add(string.Format("USE_PROTOBUF=1"));
+
+        return true;
+    }
+
+    public bool LoadJansson(TargetInfo Target)
+    {
+        string libraries_path = Path.Combine(ThirdPartyPath, "Jansson", "Libraries");
+
+        string jansson_lib;
+        if (Target.Platform == UnrealTargetPlatform.Win64)
+        {
+            jansson_lib = "jansson_d.lib";
+            if (Target.Configuration == UnrealTargetConfiguration.Development ||
+                Target.Configuration == UnrealTargetConfiguration.Shipping ||
+                Target.Configuration == UnrealTargetConfiguration.Test) {
+                jansson_lib = "jansson.lib";
+            }
+        }
+        else // TODO: Untested!
+        {
+            jansson_lib = "libjansson_d.a";
+            if (Target.Configuration == UnrealTargetConfiguration.Development ||
+                Target.Configuration == UnrealTargetConfiguration.Shipping ||
+                Target.Configuration == UnrealTargetConfiguration.Test) {
+                jansson_lib = "libjansson.a";
+            }
+        }
+
+        PublicAdditionalLibraries.Add(Path.Combine(libraries_path, jansson_lib));
+        PublicIncludePaths.Add(Path.Combine(ThirdPartyPath, "Jansson", "Includes"));
+        //Definitions.Add(string.Format("USE_JANSSON=1"));
+
+        return true;
+    }
 }
