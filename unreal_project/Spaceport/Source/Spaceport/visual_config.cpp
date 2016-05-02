70e8cb1feb4b9e48e9e2e8bec58c858774f2f89a
Zach Laine <whatwasthataddress@gmail.com>
2016-05-02 09:58:14 -0500

    Add visual_config.proto; not checked or used yet.

---
+++ unreal_project/Spaceport/Source/Spaceport/visual_config.cpp
@@ -0,0 +1,78 @@
+// WARNING: Generated code.
+// This file was generated from visual_config.proto (proto3)
+
+#ifndef BUILD_FOR_TEST
+#include "Spaceport.h"
+#endif
+
+#include "visual_config.hpp"
+
+
+
+namespace visual_config { 
+
+} 
+
+    message::visual_config::color_t to_protobuf (const visual_config::color_t& value)
+    {
+        message::visual_config::color_t retval;
+        retval.set_r(value.r);
+        retval.set_g(value.g);
+        retval.set_b(value.b);
+        return retval;
+    }
+
+    visual_config::color_t from_protobuf (const message::visual_config::color_t& msg)
+    {
+        visual_config::color_t retval;
+        retval.r = msg.r();
+        retval.g = msg.g();
+        retval.b = msg.b();
+        return retval;
+    }
+
+    message::visual_config::hex_map_t to_protobuf (const visual_config::hex_map_t& value)
+    {
+        message::visual_config::hex_map_t retval;
+        for (const auto& x : value.primary_colors) {
+            (*retval.mutable_primary_colors())[x.first] = to_protobuf(x.second);
+        }
+        for (const auto& x : value.secondary_colors) {
+            (*retval.mutable_secondary_colors())[x.first] = to_protobuf(x.second);
+        }
+        retval.set_national_border_thickness(value.national_border_thickness);
+        retval.set_province_border_thickness(value.province_border_thickness);
+        retval.set_planet_star_thickness(value.planet_star_thickness);
+        retval.set_minor_planet_scale(value.minor_planet_scale);
+        retval.set_major_planet_scale(value.major_planet_scale);
+        retval.set_star_scale(value.star_scale);
+        retval.set_mb_scale(value.mb_scale);
+        retval.set_bats_scale(value.bats_scale);
+        retval.set_sb_scale(value.sb_scale);
+        return retval;
+    }
+
+    visual_config::hex_map_t from_protobuf (const message::visual_config::hex_map_t& msg)
+    {
+        visual_config::hex_map_t retval;
+        {
+            for (const auto& x : msg.primary_colors()) {
+                retval.primary_colors[x.first] = from_protobuf(x.second);
+            }
+        }
+        {
+            for (const auto& x : msg.secondary_colors()) {
+                retval.secondary_colors[x.first] = from_protobuf(x.second);
+            }
+        }
+        retval.national_border_thickness = msg.national_border_thickness();
+        retval.province_border_thickness = msg.province_border_thickness();
+        retval.planet_star_thickness = msg.planet_star_thickness();
+        retval.minor_planet_scale = msg.minor_planet_scale();
+        retval.major_planet_scale = msg.major_planet_scale();
+        retval.star_scale = msg.star_scale();
+        retval.mb_scale = msg.mb_scale();
+        retval.bats_scale = msg.bats_scale();
+        retval.sb_scale = msg.sb_scale();
+        return retval;
+    }
---
+++ unreal_project/Spaceport/Source/Spaceport/visual_config.hpp
@@ -0,0 +1,43 @@
+// WARNING: Generated code.
+// This file was generated from visual_config (proto3)
+#pragma once
+
+#include <visual_config.pb.h>
+
+
+#include <string>
+#include <vector>
+#include <boost/container/flat_map.hpp>
+
+
+namespace visual_config { 
+
+    struct color_t
+    {
+        int r;
+        int g;
+        int b;
+    };
+
+    struct hex_map_t
+    {
+        boost::container::flat_map<std::string, color_t> primary_colors;
+        boost::container::flat_map<std::string, color_t> secondary_colors;
+        float national_border_thickness;
+        float province_border_thickness;
+        float planet_star_thickness;
+        float minor_planet_scale;
+        float major_planet_scale;
+        float star_scale;
+        float mb_scale;
+        float bats_scale;
+        float sb_scale;
+    };
+
+} 
+
+    message::visual_config::color_t to_protobuf (const visual_config::color_t& value);
+    visual_config::color_t from_protobuf (const message::visual_config::color_t& msg);
+
+    message::visual_config::hex_map_t to_protobuf (const visual_config::hex_map_t& value);
+    visual_config::hex_map_t from_protobuf (const message::visual_config::hex_map_t& msg);
