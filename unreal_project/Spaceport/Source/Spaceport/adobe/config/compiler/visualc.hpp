e999b4fefe44f63d8da50e59a3e975ed812e2ba3
Zach Laine <whatwasthataddress@gmail.com>
2016-05-06 23:56:27 -0500

    Add adobe::name_t code.

---
+++ unreal_project/Spaceport/Source/Spaceport/adobe/config/compiler/visualc.hpp
@@ -0,0 +1,57 @@
+/*
+    Copyright 2013 Adobe
+    Distributed under the Boost Software License, Version 1.0.
+    (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*/
+/*************************************************************************************************/
+
+#ifndef ADOBE_CONFIG_COMPILER_MSVC_HPP
+#define ADOBE_CONFIG_COMPILER_MSVC_HPP
+
+/*************************************************************************************************/
+
+#ifndef ADOBE_CONFIG_HPP
+#error                                                                                             \
+    "This file is intended to be included by <adobe/config.hpp> -- please use that file directly."
+#endif
+
+/*************************************************************************************************/
+
+#ifndef ADOBE_COMPILER_MSVC
+#define ADOBE_COMPILER_MSVC 1
+#endif
+
+/*************************************************************************************************/
+
+#ifndef ADOBE_TEST_MICROSOFT_NO_DEPRECATE
+#define ADOBE_TEST_MICROSOFT_NO_DEPRECATE 1
+#endif
+
+#if ADOBE_TEST_MICROSOFT_NO_DEPRECATE
+#if _MSC_VER >= 1400
+/*
+    The explanation for this check is explained at
+
+     http://stlab.adobe.com/wiki/index.php/Troubleshooting
+*/
+
+#if 0
+#ifndef _CRT_SECURE_NO_DEPRECATE
+#error                                                                                             \
+    "Microsoft 'Safe Standard C Library' is not supported. See <http://stlab.adobe.com/wiki/index.php/Troubleshooting>"
+#endif
+
+#ifndef _SCL_SECURE_NO_DEPRECATE
+#error                                                                                             \
+    "Microsoft 'Safe Standard C++ Library' is not supported. See <http://stlab.adobe.com/wiki/index.php/Troubleshooting>"
+#endif
+#endif
+#endif
+#endif
+
+
+/*************************************************************************************************/
+
+#endif
+
+/*************************************************************************************************/
---
+++ unreal_project/Spaceport/Source/Spaceport/adobe/src/name.cpp
@@ -0,0 +1,95 @@
+/*
+    Copyright 2005-2013 Adobe Systems Incorporated
+    Distributed under the MIT License (see accompanying file LICENSE_1_0_0.txt
+    or a copy at http://stlab.adobe.com/licenses.html)
+*/
+
+/****************************************************************************************************/
+
+#ifndef BUILD_FOR_TEST
+#include "Spaceport.h"
+#endif
+
+// identity
+#include <adobe/name.hpp>
+
+// stdc++
+#include <iostream>
+#include <unordered_map>
+#include <mutex>
+
+// asl
+#include <adobe/implementation/string_pool.hpp>
+
+/****************************************************************************************************/
+
+namespace {
+
+/****************************************************************************************************/
+
+constexpr std::size_t empty_hash_s = adobe::detail::name_hash("");
+
+/****************************************************************************************************/
+
+} // namespace
+
+/****************************************************************************************************/
+
+namespace adobe {
+
+/****************************************************************************************************/
+
+static_name_t::operator bool() const { return static_cast<bool>(static_cast<name_t>(*this)); }
+
+/****************************************************************************************************/
+
+bool operator<(const static_name_t& x, const static_name_t& y) { return name_t(x) < name_t(y); }
+
+/****************************************************************************************************/
+
+name_t::operator bool() const { return ptr_m != detail::empty_string_s(); }
+
+/****************************************************************************************************/
+
+const char* name_t::map_string(const char* str) {
+    if (!str || !*str)
+        return map_string(detail::empty_string_s(), empty_hash_s);
+
+    // Once fnv1a is in master we can make the hash faster
+    // with a call to the sentinel variant.
+    std::size_t hash(detail::name_hash(str, strlen(str)));
+
+    return map_string(str, hash);
+}
+
+/****************************************************************************************************/
+
+const char* name_t::map_string(const char* str, std::size_t hash) {
+    typedef std::unordered_map<std::size_t, const char*> map_t;
+    typedef std::lock_guard<std::mutex> lock_t;
+
+    static std::mutex sync_s;
+
+    lock_t lock(sync_s);
+
+    static adobe::unique_string_pool_t pool_s;
+    static map_t map_s;
+    map_t::const_iterator found(map_s.find(hash));
+
+    return found == map_s.end() ? map_s.emplace(hash, pool_s.add(str)).first->second
+                                : found->second;
+}
+
+/****************************************************************************************************/
+
+std::ostream& operator<<(std::ostream& s, const static_name_t& name) { return s << name.string_m; }
+
+/****************************************************************************************************/
+
+std::ostream& operator<<(std::ostream& s, const name_t& name) { return s << name.ptr_m; }
+
+/****************************************************************************************************/
+
+} // namespace adobe
+
+/****************************************************************************************************/
