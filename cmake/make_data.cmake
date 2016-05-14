16c0dca184f15e838770fb54482d44a2a3b8e115
Zach Laine <whatwasthataddress@gmail.com>
2016-05-14 18:08:31 -0500

    Use make_data.py instead of make_data.cmake.

--- cmake/make_data.cmake
+++
@@ -1,8 +0,0 @@
-file(READ ${nations_file} nations_contents)
-file(READ ${map_file} map_contents)
-file(READ ${oob_file} oob_contents)
-file(READ ${units_file} units_contents)
-file(READ ${scenario_file} scenario_contents)
-file(READ ${hex_map_config_file} hex_map_config_contents)
-
-configure_file(${in_file} test_data.hpp)
