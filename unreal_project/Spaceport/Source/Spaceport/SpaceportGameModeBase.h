effb0da640ea89a883b9afc7c8a1b5c88ffdc023
Zach Laine <whatwasthataddress@gmail.com>
2020-11-09 16:46:44 -0600

    Add new, empty UE 4.25 project called Spaceport.

---
+++ unreal_project/Spaceport/Source/Spaceport/SpaceportGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpaceportGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEPORT_API ASpaceportGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
