036fd796fe08047e080d5ace7018ae5e4e19b429
Zach Laine <whatwasthataddress@gmail.com>
2025-10-25 21:32:20 -0500

    Modify strings_to_string_table_csv.py to take an output file.

--- src/strings_to_string_table_csv.py
+++ src/strings_to_string_table_csv.py
@@ -3,12 +3,14 @@ import argparse
 parser = argparse.ArgumentParser(description='Takes a file with a newline-delimited list of names, and turns it into a string table suitable for use by UE5..')
 parser.add_argument('names_file', type=argparse.FileType('r', encoding='utf-8'),
                     help='Newline-delimited list of names')
+parser.add_argument('-o', required=True, type=argparse.FileType('w', encoding='utf-8'),
+                    help='CSV file to write')
 args = parser.parse_args()
 
 lines = args.names_file.readlines()
 
-print('Key,SourceString')
+args.o.write('Key,SourceString\n')
 for line in lines:
     line = line[:-1]
-    print(f'"{line}","{line}"')
+    args.o.write(f'"{line}","{line}"\n')
 
