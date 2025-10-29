ad0163f9beea353f214aa66f7aa51f7ffc9fdeb0
Zach Laine <whatwasthataddress@gmail.com>
2025-10-29 17:29:27 -0500

    Change Sstyled_button to use a Sstyled_text_block if it is given a Text arg.

--- unreal_project/Spaceport/Source/Spaceport/widgets/Sstyled_button.cpp
+++ unreal_project/Spaceport/Source/Spaceport/widgets/Sstyled_button.cpp
@@ -1,15 +1,26 @@
 #include "Sstyled_button.h"
+#include "Sstyled_text_block.h"
 
 
 void Sstyled_button::Construct(FArguments const & args_)
 {
+    bool const has_text = args_._Text.IsSet();
+
     auto const & defaults = ui_defaults();
     detail::FObjectFinder<USlateWidgetStyleAsset> style(
         *defaults.UButton_style_path_);
     SButton::FArguments args(args_);
     args.ButtonStyle(style.Object);
     args.OnClicked(this, &Sstyled_button::trampoline);
+    if (has_text) {
+        args.ContentPadding(FMargin(4, 2));
+        args.HAlign(HAlign_Center);
+        args.VAlign(VAlign_Center);
+    }
     SButton::Construct(args);
+
+    if (has_text)
+        SButton::SetContent(SNew(Sstyled_text_block).Text(args._Text));
 }
 
 FReply Sstyled_button::trampoline()
