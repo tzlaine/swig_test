03dec54886f4222f266f75381a1567380e3ea73c
Zach Laine <whatwasthataddress@gmail.com>
2025-10-31 22:39:22 -0500

    Add DirectoryWatcher to public build dependencies to enable DebugEditor builds (DebugGame builds mysteriously do not build).

--- unreal_project/Spaceport/Source/Spaceport/Spaceport.Build.cs
+++ unreal_project/Spaceport/Source/Spaceport/Spaceport.Build.cs
@@ -5,28 +5,28 @@ using System.IO;
 
 public class Spaceport : ModuleRules
 {
-	private string ModulePath
-	{
-		get { return ModuleDirectory; }
-	}
+    private string ModulePath
+    {
+        get { return ModuleDirectory; }
+    }
 
-	private string ThirdPartyPath
-	{
-		get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
-	}
+    private string ThirdPartyPath
+    {
+        get { return Path.GetFullPath(Path.Combine(ModulePath, "../../ThirdParty/")); }
+    }
 
-	public Spaceport(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+    public Spaceport(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "DirectoryWatcher" });
+
+        PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 
         CppStandard = CppStandardVersion.Cpp20;
 
