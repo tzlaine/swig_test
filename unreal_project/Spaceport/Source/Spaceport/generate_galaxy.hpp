74e6141e51750771537433bb2d187dafd4e854e8
Zach Laine <whatwasthataddress@gmail.com>
2025-11-15 02:52:04 -0600

    Move several small utility types and functions from generate_galaxy.hpp to nre header map_util.hpp.

--- unreal_project/Spaceport/Source/Spaceport/generate_galaxy.hpp
+++ unreal_project/Spaceport/Source/Spaceport/generate_galaxy.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "constants.hpp"
 #include "game_data_t.hpp"
+#include "map_util.hpp"
 #include "rng.hpp"
 
 #if !defined(BUILD_FOR_TEST)
@@ -17,39 +17,6 @@ struct game_state_t;
 template<typename T>
 struct concurrent_queue;
 struct task_system;
-
-// TODO: Move these.
-struct point_2d
-{
-    double x;
-    double y;
-};
-
-inline point_2d operator-(point_2d pt1, point_2d pt2)
-{
-    return {pt1.x - pt2.x, pt1.y - pt2.y};
-}
-
-inline bool within(point_2d pt1, point_2d pt2, double dist)
-{
-    auto const delta = pt1 - pt2;
-    return (delta.x * delta.x + delta.y * delta.y + 0.001) < (dist * dist);
-}
-
-inline point_2d hex_position(hex_coord_t hc, int map_height)
-{
-    point_2d retval;
-    retval.x = hc.x * 1.5;
-    retval.y = (map_height - 1 - hc.y) * 2 * sin_60;
-    if ((hc.x + 1000) % 2 == 1)
-        retval.y -= sin_60;
-    return retval;
-}
-
-inline double degrees_to_radians(double d)
-{
-    return d / 180.0 * std::numbers::pi;
-}
 
 namespace generation {
     namespace detail {
