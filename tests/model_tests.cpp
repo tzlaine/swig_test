3b9440d37a5a161119287ed3828fe12989ec2b76
Zach Laine <whatwasthataddress@gmail.com>
2025-11-17 19:03:34 -0600

    Add timings to the model save/load test.

--- tests/model_tests.cpp
+++ tests/model_tests.cpp
@@ -5,6 +5,15 @@
 #include <gtest/gtest.h>
 
 
+void print_time(auto prev_time)
+{
+    auto const now = std::chrono::high_resolution_clock::now();
+    auto const elapsed = now - prev_time;
+    auto const ms =
+        std::chrono::duration_cast < std::chrono::milliseconds>(elapsed);
+    std::cout << std::format(" ({})\n", ms);
+}
+
 TEST(model_tests, generate_save_load)
 {
     std::cout << "generate_save_load test\n";
@@ -12,29 +21,38 @@ TEST(model_tests, generate_save_load)
     model m;
     concurrent_queue<int> q;
     std::atomic_bool complete = false;
-    std::cout << "generating\n";
+    std::cout << "generating";
+    auto start_time = std::chrono::high_resolution_clock::now();
     m.generate_galaxy(default_game_start_params(), q, complete);
+    print_time(start_time);
 
     std::filesystem::path path = "save_0";
     std::filesystem::remove(path);
 
-    std::cout << "saving\n";
+    std::cout << "scheduling save";
+    start_time = std::chrono::high_resolution_clock::now();
     m.save(path);
+    print_time(start_time);
 
     auto const pre_save_game_state = m.game_state();
 
     // Wait for it to be written before reading.
-    std::cout << "waiting for save file to appear\n";
+    std::cout << "waiting for save file to appear";
+    start_time = std::chrono::high_resolution_clock::now();
     while (!std::filesystem::exists(path)) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
+    print_time(start_time);
 
-    std::cout << "loading\n";
+    std::cout << "loading";
+    start_time = std::chrono::high_resolution_clock::now();
     m.load(path);
+    print_time(start_time);
 
     auto const post_load_game_state = m.game_state();
 
-    std::cout << "comparing game_states\n";
+    std::cout << "comparing game_states";
+    start_time = std::chrono::high_resolution_clock::now();
     game_state_t const & before = *pre_save_game_state;
     game_state_t const & after = *post_load_game_state;
     EXPECT_EQ(before.map_width, after.map_width);
@@ -47,4 +65,5 @@ TEST(model_tests, generate_save_load)
     EXPECT_EQ(before.planets, after.planets);
     EXPECT_EQ(before.nations.size(), after.nations.size());
     EXPECT_EQ(before.nations, after.nations);
+    print_time(start_time);
 }
