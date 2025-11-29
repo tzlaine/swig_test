d83188044260da52af2e04627bb298222808fbf3
Zach Laine <whatwasthataddress@gmail.com>
2025-11-29 12:22:00 -0600

    Remove EnhancedInputUserSettings.sav from the list of published save files.

--- unreal_project/Spaceport/Source/Spaceport/Agame_mode_base.cpp
+++ unreal_project/Spaceport/Source/Spaceport/Agame_mode_base.cpp
@@ -32,6 +32,16 @@ void Agame_mode_base::load_and_start_game_Implementation(
 void Agame_mode_base::publish_save_files()
 {
     TArray<FString> saves = find_save_files();
+
+    // Filter out known non-game .sav files.
+    auto const first = begin(saves);
+    auto const last = end(saves);
+    auto it = std::remove_if(first, last, [](auto const & e) {
+        return e == TEXT("EnhancedInputUserSettings");
+    });
+    if (it != last)
+        saves.SetNum(saves.Num() - (last - it));
+
     UE_LOG(
         LogTemp,
         Log,
