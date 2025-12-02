eec99a6ff950c980670da1f72fffd95e22454584
Zach Laine <whatwasthataddress@gmail.com>
2025-12-02 17:48:35 -0600

    Grooming.

--- unreal_project/Spaceport/Source/Spaceport/widgets/Spip_rotator_button.cpp
+++ unreal_project/Spaceport/Source/Spaceport/widgets/Spip_rotator_button.cpp
@@ -74,8 +74,7 @@ void Spip_rotator_button::Construct(FArguments const & args)
 {
     int const size = args._settings.Num();
 
-    auto * pc = player_controller_base();
-    UMaterialInterface * base_material = pc->materials().rotator_pip_;
+    UMaterialInterface * base_material = materials().rotator_pip_;
     pip_material_.Reset(
         UMaterialInstanceDynamic::Create(base_material, nullptr));
     pip_material_->SetScalarParameterValue(TEXT("num_pips"), size);
