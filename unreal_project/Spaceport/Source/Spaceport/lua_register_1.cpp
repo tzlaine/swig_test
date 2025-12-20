368ed71d0f7ee59bbcfcfb6191b1cc8e2696db1a
Zach Laine <whatwasthataddress@gmail.com>
2025-12-20 15:51:32 -0600

    Provide several overloads for planet_effect_t.new() in the Lua code, since assigning to an adobe::name_t from a Lua string does not work (when done entirely within Lua code -- not across the C++/Lua binding boundary).

--- unreal_project/Spaceport/Source/Spaceport/lua_register_1.cpp
+++ unreal_project/Spaceport/Source/Spaceport/lua_register_1.cpp
@@ -11,5 +11,16 @@ void detail::lua_register_1(sol::state & l)
     REGISTER_GAME_DATA_TYPE(l, fleet_position_t);
     REGISTER_GAME_DATA_TYPE(l, fleet_t);
     REGISTER_GAME_DATA_TYPE(l, fleets_t);
+
     REGISTER_GAME_DATA_TYPE(l, planet_effect_t);
+    planet_effect_t_type["new"] = sol::overload(
+        [](char const * name) { return planet_effect_t{adobe::name_t(name)}; },
+        [](char const * name, char const * reason) {
+            return planet_effect_t{adobe::name_t(name), adobe::name_t(reason)};
+        },
+        [](char const * name, char const * reason, float value) {
+            return planet_effect_t{
+                adobe::name_t(name), adobe::name_t(reason), value};
+        },
+        &detail::make_default<planet_effect_t>);
 }
