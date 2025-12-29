dae0d7a498aa3e1ee0eacfc750f4fa1684a28a81
Zach Laine <whatwasthataddress@gmail.com>
2025-12-29 13:39:37 -0600

    Cruft removal.

--- unreal_project/Spaceport/Source/Spaceport/space_creator_actor_config.hpp
+++ unreal_project/Spaceport/Source/Spaceport/space_creator_actor_config.hpp
@@ -3,8 +3,6 @@
 class AActor;
 class AStaticMeshActor;
 class Amap_system;
-struct loaded_textures;
-struct loaded_material_interfaces;
 struct planet_t;
 struct star_t;
 struct system_t;
@@ -17,16 +15,14 @@ void configure_map_star(
 void configure_system_star(AActor * star_actor, star_t const & star);
 
 void configure_rocky_oxidized_planet(
-    AActor * planet_actor, planet_t const & planet, loaded_textures & textures);
+    AActor * planet_actor, planet_t const & planet);
 
 void configure_rocky_reduced_or_carbon_rich_planet(
-    AActor * planet_actor, planet_t const & planet, loaded_textures & textures);
+    AActor * planet_actor, planet_t const & planet);
 
 void configure_high_temperature_planet(
-    AActor * planet_actor, planet_t const & planet, loaded_textures & textures);
+    AActor * planet_actor, planet_t const & planet);
 
-void configure_gas_giant_planet(
-    AActor * planet_actor, planet_t const & planet, loaded_textures & textures);
+void configure_gas_giant_planet(AActor * planet_actor, planet_t const & planet);
 
-void configure_ice_giant_planet(
-    AActor * planet_actor, planet_t const & planet, loaded_textures & textures);
+void configure_ice_giant_planet(AActor * planet_actor, planet_t const & planet);
